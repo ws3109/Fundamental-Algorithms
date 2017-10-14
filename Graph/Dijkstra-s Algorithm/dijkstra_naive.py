d07d462389088993fb3e759e8b1a2dc4c4779623
ws3109 <ws3109@g.rit.edu>
2017-10-14 12:51:30 -0400

    Update dijkstra_naive.py

--- Graph/Dijkstra-s Algorithm/dijkstra_naive.py
+++ Graph/Dijkstra-s Algorithm/dijkstra_naive.py
@@ -1,56 +1,43 @@
-import collections
+from collections import *
 import random
 
 class Graph:
     def __init__(self):
-        self.nodes = set()
-        self.edges = collections.defaultdict(list)
-        self.distances = {}
-
-    def add_node(self, value):
-        self.nodes.add(value)
+        self.adjList = defaultdict(list)
 
     def add_edge(self, from_node, to_node, distance):
-        self.edges[from_node].append(to_node)
-        self.distances[(from_node, to_node)] = distance
+        self.adjList[from_node].append((to_node, distance))
+        self.adjList[to_node].append((from_node, distance))
 
-    def __str__(self):
-        return '\n'.join([' '.join([str(a[0]), str(a[1]),str(self.distances[a])])for a in self.distances])
-
-def generateGraph(n, threshold = 0.3, low = 10, high = 1000):
+def generateGraph(n, threshold = 0.3, low = 10, high = 100):
     g = Graph()
     for i in range(n):
-        g.add_node(i)
-    for i in range(n):
-        for j in range(n):
+        for j in range(i+1, n):
             if random.uniform(0, 1) <= threshold:
                 g.add_edge(i, j, random.randint(low, high))
     return g
 
 def dijkstra(graph, initial):
     INF = float('inf')
-    unvisited_nodes = set(graph.nodes)
-    dist, path = {}, {}
-    for node in unvisited_nodes:
-        dist[node] = INF if node != initial else 0
-        path[node] = [] if node != initial else [node]
+    dist, path, unvisited_nodes = {}, {}, set(graph.adjList.keys())
+    for u in graph.adjList:
+        dist[u] = INF if u != initial else 0
+        path[u] = [] if u != initial else [u]
     while len(unvisited_nodes) > 0:
-        min_node = None
-        for node in unvisited_nodes:
-            min_node = node if (min_node is None or dist[node] < dist[min_node]) else min_node
-        if dist[min_node] == INF:
-            break
-        unvisited_nodes.remove(min_node)
-        for neighbor in graph.edges[min_node]:
-            new_dist =  dist[min_node] + graph.distances[(min_node, neighbor)]
-            if new_dist < dist[neighbor]:
-                dist[neighbor], path[neighbor] = new_dist, path[min_node] + [neighbor]
+        u = min(unvisited_nodes, key = lambda x: dist[x])
+        unvisited_nodes.remove(u)
+        for v, edge_cost in graph.adjList[u]:
+            if dist[v] > dist[u] + edge_cost:
+                dist[v], path[v] = dist[u] + edge_cost, path[u] + [v]
     return dist, path
 
 def test():
-    n, initial = 100, 0
+    n, initial = 10, 0
     g = generateGraph(n)
     dist, path = dijkstra(g, initial)
+    for u in g.adjList:
+        for v, edge_cost in g.adjList[u]:
+            assert dist[v] <= dist[u] + edge_cost
     for node in dist:
         print("Node %d: " % (node) +'->'.join(map(str, path[node])) + ': '+ str(dist[node]))
 
